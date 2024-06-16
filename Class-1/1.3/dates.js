cdee1fefe5095ccfe85fe4dffd6acabfd0f82180
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-17 00:42:05 +0530

    Added Dates and Classes

---
+++ Class-1/1.3/dates.js
@@ -0,0 +1,5 @@
+const currentDate = new Date();
+
+console.log(currentDate.getDate()); // for current date
+console.log(currentDate.getMonth() + 1); // for current month + 1, because the month index starts from '0'
+console.log(currentDate.getFullYear()); // for full year i.e. 2024
--- Class-1/1.3/main.js
+++ Class-1/1.3/main.js
@@ -1,22 +1,53 @@
-// classes in javascript
+// const dog = {
+//   name: "doggie",
+//   leg: 2,
+//   speaks: "bhow bhow",
+// };
 
-const dog = {
-  name: "doggie",
-  leg: 2,
-  speaks: "bhow bhow",
-};
+// const cat = {
+//   name: "catt",
+//   leg: 4,
+//   speaks: "meow meow",
+// };
 
-const cat = {
-  name: "catt",
-  leg: 4,
-  speaks: "meow meow",
-};
+// function printStr(animal) {
+//   console.log(
+//     "animal is " + animal["name"] + " " + "speaks " + animal["speaks"]
+//   );
+// }
 
-function printStr(animal) {
-  console.log(
-    "animal is " + animal["name"] + " " + "speaks " + animal["speaks"]
-  );
+// printStr(dog);
+// printStr(cat);
+
+// CLASSES //
+
+// every name has name, leg count, speak
+class Animal {
+  constructor(name, legCount, speaks) {
+    this.name = name;
+    this.legCount = legCount;
+    this.speaks = speaks;
+  }
+  static // not associated with a object, these are associated with the class itself
+  speak() {
+    console.log(" hi there " + this.speaks);
+  }
 }
 
-printStr(dog);
-printStr(cat);
+// let dog1 = {
+//   name: "dog",
+//   legCount: 4,
+//   speaks: "bhow bhow",
+// };
+
+// let cat1 = {
+//   name: "cat",
+//   legCount: 2,
+//   speaks: "mew",
+// };
+
+let dog = new Animal("dog", 4, "bhow bhow"); // create object
+let cat1 = new Animal("cat", 2, "mew");
+dog.speak();
+cat1.speak(); // call function on object
+// class, object, this is creating an object of class
