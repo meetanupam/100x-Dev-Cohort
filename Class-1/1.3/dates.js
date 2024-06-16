f528289494915a0f58c9f65cc46adfb2dd992c74
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-17 00:55:27 +0530

    Json.parse and Json.stringify added

--- Class-1/1.3/dates.js
+++ Class-1/1.3/dates.js
@@ -1,5 +1,18 @@
-const currentDate = new Date();
+// const currentDate = new Date();
 
-console.log(currentDate.getDate()); // for current date
-console.log(currentDate.getMonth() + 1); // for current month + 1, because the month index starts from '0'
-console.log(currentDate.getFullYear()); // for full year i.e. 2024
+// console.log(currentDate.getDate()); // for current date
+// console.log(currentDate.getMonth() + 1); // for current month + 1, because the month index starts from '0'
+// console.log(currentDate.getFullYear()); // for full year i.e. 2024
+
+// objects
+
+const users = '{ "name": "anupam", "age": "23", "gender": "male" }';
+// for json.parse method you have to add the quote to the key also with the value
+// console.log(users["name"]);
+// console.log(users["gender"]);
+
+// JSON.parse
+// JSON.stringify
+
+const user = JSON.parse(users);
+console.log(user["gender"]);
---
+++ Class-1/1.3/json.js
@@ -0,0 +1,19 @@
+// JSON.parse //
+
+// const users = '{ "name": "anupam", "age": "23", "gender": "male" }';
+// for json.parse method you have to add the quote to the key also with the value
+// console.log(users["name"]);
+// console.log(users["gender"]);
+
+// const user = JSON.parse(users);
+// console.log(user["gender"]);
+
+// JSON.stringify //
+
+const user = {
+  name: "anupam",
+  gender: "male",
+};
+
+const finalString = JSON.stringify(user);
+console.log(finalString);
